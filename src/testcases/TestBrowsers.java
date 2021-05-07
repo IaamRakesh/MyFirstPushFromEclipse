acdf02e1ef15c26a9171fe8cc4ec48ec6fd21b6a
Rakesh K <SESA543238@se.com>
2021-05-07 10:00:23 +0530

    ModifiedTestBrow

--- src/testcases/TestBrowsers.java
+++ src/testcases/TestBrowsers.java
@@ -1,6 +1,7 @@
 package testcases;
 
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.edge.EdgeDriver;
 //import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.ie.InternetExplorerDriver;
 // import org.openqa.selenium.firefox.FirefoxDriver;
@@ -15,17 +16,17 @@ public class TestBrowsers {
 		
 		// FirefoxDriver driver= new FirefoxDriver();
 		
-		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\SESA543238\\Desktop\\Udemy Learning\\Selenium Executables\\chromedriver_win32 (2)\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\SESA543238\\Desktop\\Udemy Learning\\Selenium Executables\\chromedriver_win32 (2)\\chromedriver.exe");
 		
-		//ChromeDriver driver = new ChromeDriver();
+		ChromeDriver driver = new ChromeDriver();
 		
 		//System.setProperty("webdriver.ie.driver", "C:\\Users\\SESA543238\\Desktop\\Udemy Learning\\Selenium Executables\\IEDriverServer_x64_3.150.1 (2)\\IEDriverServer.exe");
 		
 		//InternetExplorerDriver driver = new InternetExplorerDriver();
 		
-		System.setProperty("webdriver.edge.driver", "C:\\Users\\SESA543238\\Desktop\\Udemy Learning\\Selenium Executables\\edgedriver_win64\\msedgedriver.exe");
+		//System.setProperty("webdriver.edge.driver", "C:\\Users\\SESA543238\\Desktop\\Udemy Learning\\Selenium Executables\\edgedriver_win64\\msedgedriver.exe");
 		
-		EdgeDriver driver= new EdgeDriver();
+		//EdgeDriver driver= new EdgeDriver();
 		
 		
 		driver.get("http://www.way2automation.com/");
